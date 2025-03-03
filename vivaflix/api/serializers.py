8c558a33563644b7ddf19f2f6e191800404a9679
RAHHHKETT <austriakeith08@gmail.com>
2025-03-03 23:02:08 +0800

    Update serializers.py

--- vivaflix/api/serializers.py
+++ vivaflix/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
-from .models import Movie, Watchlist, Rating
+from .models import Movie, Watchlist, Rating, Video
 from django.contrib.auth.models import User
-from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.password_validation import validate_password
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ['id', 'title', 'image_link', 'description']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
