f4c3b25d36f248c9106d6b980db7f3e71fdee9a3
RAHHHKETT <austriakeith08@gmail.com>
2025-03-03 23:00:42 +0800

    Update models.py

--- vivaflix/api/models.py
+++ vivaflix/api/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.shortcuts import render
 
+
+class Image(models.Model):
+    key = models.CharField(max_length=100, help_text="The public id of the uploaded file")
+    url = models.URLField(max_length=200)
+    name = models.CharField(max_length=100, help_text='The original name of the uploaded image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+def upload_media(request):
+    return render(request, 'upload_media.html')
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    video_link = models.URLField(max_length=200, blank=True, null=True)
+    image_link = models.URLField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/', blank=True)
